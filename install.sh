9c84b74d847b7996b3f802e0f434bdabe82e3904
Hiiragi Yuriko <hiiragiyuriko@gmail.com>
2020-07-02 17:56:44 +0800

    Always install the latest version of V2Ray

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ create_mainfest_file(){
     echo "应用名称：${IBM_APP_NAME}"
     read -p "请输入你的应用内存大小(默认256)：" IBM_MEM_SIZE
     if [ -z "${IBM_MEM_SIZE}" ];then
-	IBM_MEM_SIZE=256
+    IBM_MEM_SIZE=256
     fi
     echo "内存大小：${IBM_MEM_SIZE}"
-	UUID=$(cat /proc/sys/kernel/random/uuid)
+    UUID=$(cat /proc/sys/kernel/random/uuid)
     echo "生成随机UUID：${UUID}"
-	WSPATH=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
-	echo "生成随机WebSocket路径：${WSPATH}"
+    WSPATH=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
+    echo "生成随机WebSocket路径：${WSPATH}"
     
     cat >  ${SH_PATH}/IBMYes/v2ray-cloudfoundry/manifest.yml  << EOF
     applications:
@@ -25,34 +25,34 @@ create_mainfest_file(){
 EOF
 
     cat >  ${SH_PATH}/IBMYes/v2ray-cloudfoundry/v2ray/config  << EOF
-	{
-		"inbounds": [
-			{
-				"port": 8080,
-				"protocol": "vmess",
-				"settings": {
-					"clients": [
-						{
-							"id": "${UUID}",
-							"alterId": 4
-						}
-					]
-				},
-				"streamSettings": {
-					"network":"ws",
-					"wsSettings": {
-						"path": "${WSPATH}"
-					}
-				}
-			}
-		],
-		"outbounds": [
-			{
-				"protocol": "freedom",
-				"settings": {}
-			}
-		]
-	}
+    {
+        "inbounds": [
+            {
+                "port": 8080,
+                "protocol": "vmess",
+                "settings": {
+                    "clients": [
+                        {
+                            "id": "${UUID}",
+                            "alterId": 4
+                        }
+                    ]
+                },
+                "streamSettings": {
+                    "network":"ws",
+                    "wsSettings": {
+                        "path": "${WSPATH}"
+                    }
+                }
+            }
+        ],
+        "outbounds": [
+            {
+                "protocol": "freedom",
+                "settings": {}
+            }
+        ]
+    }
 EOF
     echo "配置完成。"
 }
@@ -63,7 +63,30 @@ clone_repo(){
     cd IBMYes
     git submodule update --init --recursive
     cd v2ray-cloudfoundry/v2ray
-    chmod +x *
+    # Upgrade V2Ray to the latest version
+    rm v2ray v2ctl
+    
+    # Script from https://github.com/v2fly/fhs-install-v2ray/blob/master/install-release.sh
+    # Get V2Ray release version number
+    TMP_FILE="$(mktemp)"
+    if ! curl -s -o "$TMP_FILE" 'https://api.github.com/repos/v2fly/v2ray-core/releases/latest'; then
+        rm "$TMP_FILE"
+        echo 'error: 获取最新V2Ray版本号失败。请重试'
+        exit 1
+    fi
+    RELEASE_LATEST="$(sed 'y/,/\n/' "$TMP_FILE" | grep 'tag_name' | awk -F '"' '{print $4}')"
+    rm "$TMP_FILE"
+    echo "当前最新V2Ray版本为$RELEASE_LATEST"
+    # Download latest release
+    DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/$RELEASE_LATEST/v2ray-linux-64.zip"
+    if ! curl -L -H 'Cache-Control: no-cache' -o "latest-v2ray.zip" "$DOWNLOAD_LINK"; then
+        echo 'error: 下载V2Ray失败，请重试'
+        return 1
+    fi
+    unzip latest-v2ray.zip v2ray v2ctl geoip.dat geosite.dat
+    rm latest-v2ray.zip
+    
+    chmod +x v2ray v2ctl
     cd ${SH_PATH}/IBMYes/v2ray-cloudfoundry
     echo "初始化完成。"
 }
@@ -76,7 +99,7 @@ install(){
     ibmcloud cf push
     echo "安装完成。"
     echo "生成的随机 UUID：${UUID}"
-	echo "生成的随机 WebSocket路径：${WSPATH}"
+    echo "生成的随机 WebSocket路径：${WSPATH}"
 }
 
 clone_repo
