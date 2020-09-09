4311d1042fd991516832b0af1b0716d1cfb0e337
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2020-09-09 09:19:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ create_mainfest_file(){
       memory: ${IBM_MEM_SIZE}M
 EOF
 
-    cat >  ${SH_PATH}/IBMYes/v2ray-cloudfoundry/v2ray/config.json  << EOF
+    cat >  ${SH_PATH}/IBMYes-edit-from-CCChieh/v2ray-cloudfoundry/v2ray/config.json  << EOF
     {
         "inbounds": [
             {
